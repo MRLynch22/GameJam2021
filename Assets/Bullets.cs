c44eaf94bed6709927a3c744f167a67e1ba5f06f
LAPTOP-5P1KC3DP\agyor <agyory.work@gmail.com>
2021-12-09 22:13:03 +1030

    Added bullets and character look at when firing

--- Assets/Bullets.cs
+++ Assets/Bullets.cs
@@ -5,7 +5,9 @@ using UnityEngine;
 public class Bullets : MonoBehaviour
 {
 
-    public GameObject carrotBulletPrefab; 
+    public GameObject carrotBulletPrefab;
+
+    public Transform bulletPoint; 
 
 
     // Start is called before the first frame update
@@ -17,10 +19,28 @@ public class Bullets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Mouse0))
+        
+
+        
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
         {
+
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, 100))
+            {
+                transform.LookAt(hit.point);
+                Debug.Log(hit.transform.name);
+                Debug.Log("hit");
+            }
+
             Debug.Log("Pew Pew");
-            GameObject _bullet = Instantiate(carrotBulletPrefab, transform.position, Quaternion.identity);
+            GameObject _bullet = Instantiate(carrotBulletPrefab, bulletPoint.position, Quaternion.identity);
+            _bullet.GetComponent<Rigidbody>().AddForce(Vector3.forward * 10f, ForceMode.Impulse); 
         }
+
+        
+
     }
 }
