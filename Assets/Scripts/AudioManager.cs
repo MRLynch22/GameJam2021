203c15c7b1569b371afff6b39be3eeb8378dec8f
CameronMaymanAIE <s210086@students.aie.edu.au>
2021-12-09 16:17:53 +1030

    Made Audio Manager script.

---
+++ Assets/Scripts/AudioManager.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    private AudioClip backgroundMusic;
+    private AudioClip mainMenuMusic;
+
+    private AudioClip pauseMenu;
+    public AudioClip gameOver;
+
+    private AudioClip roundStarted;
+    private AudioClip roundWon;
+
+    private AudioClip enemyDies;
+    private AudioClip enemyHit;
+    private AudioClip playerHit;
+
+    private AudioClip[] santaLines;
+
+    private AudioClip[] coalGrenade;
+    private AudioClip dash;
+    private AudioClip shootCarrots;
+
+}
