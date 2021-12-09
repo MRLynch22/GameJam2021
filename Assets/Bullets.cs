011ebf5db840affcb63c040d45c5fa0a1faf846a
AIE\s210006 <s210006@students.aie.edu.au>
2021-12-09 20:05:22 +1030

    Added force to prefab bullet

--- Assets/Bullets.cs
+++ Assets/Bullets.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class Bullets : MonoBehaviour
 {
+
+    public GameObject carrotBulletPrefab; 
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -13,6 +17,10 @@ public class Bullets : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if(Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Debug.Log("Pew Pew");
+            GameObject _bullet = Instantiate(carrotBulletPrefab, transform.position, Quaternion.identity);
+        }
     }
 }
